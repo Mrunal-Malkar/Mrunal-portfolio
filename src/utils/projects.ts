6bcfc5721dd26992f1edf865c006df751c8d67cf
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-04-26 16:15:02 +0530

    feat: add multiple project entries and images to projects utility; enhance project details

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -11,13 +11,67 @@ export const AllProject = [
     image2: "musicalimage",
     livelink2: "https://musical-eosin.vercel.app/",
   },
+  {
+    name1: "amazon-clone",
+    gihub1: "https://github.com/Mrunal-Malkar/Amazon-clone-using-Html-CSS",
+    about1: "Built stunning Amazon clone using HTML and CSS only!",
+    image1: "amazon-css.jpg",
+    livelink1: "https://mrunal-malkar.github.io/Amazon-clone-using-Html-CSS/",
+    name2: "BookStore website",
+    about2: "Built using MERN stack, deployed on vercel and railway",
+    gihub2: "https://github.com/Mrunal-Malkar/Bookstore-website-MERN-stack",
+    image2: "BookStore.png",
+    livelink2: "https://bookstore-website-mern-stack.vercel.app/",
+  },
+  {
+    name1: "Contact-app",
+    gihub1: "https://github.com/Mrunal-Malkar/Contact-app-using-firebase-and-react",
+    about1: "This is the contact app made using react and firebase.",
+    image1: "contact-app-firebase.png",
+    livelink1: "",
+    name2: "Font-Awesome",
+    about2: "font awesome website clone using bootstrap and css",
+    gihub2: "https://github.com/Mrunal-Malkar/Font-awesome-website-clone-using-bootstrap-CSS",
+    image2: "fontawesomeclone.jpg",
+    livelink2: "",
+  },
+  {
+    name1: "Rock-paper-scissors",
+    gihub1: "https://github.com/Mrunal-Malkar/Rock-Paper-Scissor-game-using-Javascript-js-",
+    about1: "Rock paper scissors game using vanilla css and javascript!",
+    image1: "rockpaperscissorgame.jpg",
+    livelink1: "https://mrunal-malkar.github.io/Rock-Paper-Scissor-game-using-Javascript-js-/",
+    name2: "Todo List App",
+    about2: "this is where you store your todos! built using react with super clean UI.",
+    gihub2: "https://github.com/Mrunal-Malkar/To-Do-List-with-React",
+    image2: "todolist.jpg",
+    livelink2: "",
+  },
 ];
 
-// name1: "",
+//ADD ONE PROJECT BOILERPLATE
+
+//{ name1: "",
 // gihub1: "",
 // about1: "",
 // image1: "",
-// livelink1: "",
+// livelink1: ""},
+
+
+//ADD TWO PROJECTS BOILERPLATE
+
+// {
+//   name1: "",
+//   gihub1: "",
+//   about1: "",
+//   image1: "",
+//   livelink1: "",
+//   name2: "",
+//   about2: "",
+//   gihub2: "",
+//   image2: "",
+//   livelink2: "",
+// },
 
 function getTopProjects() {
   if (AllProject.length == 0) {
@@ -36,8 +90,12 @@ function getTopProjects() {
       val.about2 &&
       val.gihub2
     ) {
-      no.push("1", "1");
-      return val;
+      no.push("1");
+      if(no.length>=3){
+       return { name1: val.name1, about1: val.about1, gihub1: val.gihub1, image1: val.image1, livelink1: val.livelink1 }
+      }else{
+        return val;
+      }
     } else if (val.name1 && val.gihub1 && val.about1) {
       no.push("1");
       return val;

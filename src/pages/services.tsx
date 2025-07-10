b6d49c725444b803a9c33aee07b67550064bf34e
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2025-07-11 01:30:29 +0530

    added whyonline section

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,6 +1,7 @@
 import Navbar2 from "../components/navbar2";
 import "../../public/home.css";
 import LandingPage from "../components/landingpage";
+import WhyGoOnline from "../components/whyonline";
 
 const Services = () => (
   <>
@@ -8,7 +9,7 @@ const Services = () => (
     <Navbar2 />
     {/* landing page section */}
     <LandingPage/>
-    
+    <WhyGoOnline />
   </>
 );
 

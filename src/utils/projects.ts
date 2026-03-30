7aebcfdd4a6cdf9f88479c55ed0af179e4f64f45
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2026-03-30 12:31:53 +0530

    add:stackRescue project && remove:previous projects

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -8,6 +8,13 @@ interface ProjectType {
 
 export const AllProject: ProjectType[] = [
   {
+    name:"StackRescue",
+    github:"https://github.com/Mrunal-Malkar/StackRescue",
+    about:"Let your halfBuilt Project get Rescued.",
+    image:"projectimage/stackrescue.png",
+    livelink:"https://stack-rescue.vercel.app/"
+  },
+  {
     name: "ChAtTeR",
     github: "https://github.com/Mrunal-Malkar/chatter--Mern-stack-chat-app",
     about: "Real time chat application",
@@ -20,53 +27,18 @@ export const AllProject: ProjectType[] = [
     about: "Listen according to listeners!",
     image: "projectimage/musicalimage.png",
     livelink: "https://musical-eosin.vercel.app/",
-  },
-  {
-    name: "Amazon Clone",
-    github: "https://github.com/Mrunal-Malkar/Amazon-clone-using-Html-CSS",
-    about: "Built stunning Amazon clone using HTML and CSS only!",
-    image: "projectimage/amazon-css.jpg",
-    livelink: "https://mrunal-malkar.github.io/Amazon-clone-using-Html-CSS/",
-  },
-  {
-    name: "BookStore Website",
-    github: "https://github.com/Mrunal-Malkar/Bookstore-website-MERN-stack",
-    about: "Built using MERN stack, deployed on Vercel and Railway",
-    image: "projectimage/BookStore.png",
-    livelink: "https://bookstore-website-mern-stack.vercel.app/",
-  },
-  {
-    name: "Contact App",
-    github: "https://github.com/Mrunal-Malkar/Contact-app-using-firebase-and-react",
-    about: "Contact app made using React and Firebase.",
-    image: "projectimage/contact-app-firebase.png",
-    livelink: "",
-  },
-  {
-    name: "Font-Awesome Clone",
-    github: "https://github.com/Mrunal-Malkar/Font-awesome-website-clone-using-bootstrap-CSS",
-    about: "Font Awesome website clone using Bootstrap and CSS",
-    image: "projectimage/fontawesomeclone.jpg",
-    livelink: "",
-  },
-  {
-    name: "Rock Paper Scissors",
-    github: "https://github.com/Mrunal-Malkar/Rock-Paper-Scissor-game-using-Javascript-js-",
-    about: "Rock paper scissors game using Vanilla CSS and JavaScript!",
-    image: "projectimage/rockpaperscissorgame.jpg",
-    livelink: "https://mrunal-malkar.github.io/Rock-Paper-Scissor-game-using-Javascript-js-/",
-  },
-  {
-    name: "Todo List App",
-    github: "https://github.com/Mrunal-Malkar/To-Do-List-with-React",
-    about: "Where you store your todos! Built with React and clean UI.",
-    image: "projectimage/todolist.jpg",
-    livelink: "",
   },
 ];
 
 
 export const LatestWorkedProject: ProjectType[] = [
+    {
+    name:"StackRescue",
+    github:"https://github.com/Mrunal-Malkar/StackRescue",
+    about:"Let your halfBuilt Project get Rescued.",
+    image:"projectimage/stackrescue.png",
+    livelink:"https://stack-rescue.vercel.app/"
+  },
   {
     name: "ChAtTeR",
     github: "https://github.com/Mrunal-Malkar/chatter--Mern-stack-chat-app",
@@ -80,12 +52,5 @@ export const LatestWorkedProject: ProjectType[] = [
     about: "Listen according to listeners!",
     image: "projectimage/musicalimage.png",
     livelink: "https://musical-eosin.vercel.app/",
-  },
-  {
-    name: "amazon-clone",
-    github: "https://github.com/Mrunal-Malkar/Amazon-clone-using-Html-CSS",
-    about: "Built stunning Amazon clone using HTML and CSS only!",
-    image: "projectimage/amazon-css.jpg",
-    livelink: "https://mrunal-malkar.github.io/Amazon-clone-using-Html-CSS/",
   },
 ];
